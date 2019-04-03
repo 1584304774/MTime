b829346ddb45b3e07011ecf776dc7196409a9fa6
褚荣浩 <1512344358@qq.com>
2019-04-03 14:13:52 +0800

    首页修改

--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-<<<<<<< HEAD
-import find from './find/index'
-=======
 import Find from './find/index'
 import Home from './home/index'
->>>>>>> chu
 
 Vue.use(Vuex)
 export default new Vuex.Store({
   modules: {
-<<<<<<< HEAD
-    find
-=======
     Find,
     Home
->>>>>>> chu
   }
 })
