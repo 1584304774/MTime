614b6436e32448576d25ce269b41aa6705e16bac
Your Name <you@example.com>
2019-04-03 14:14:33 +0800

    4-3第一次修改

--- src/api/home/api.js
+++ src/api/home/api.js
@@ -1,2 +1,2 @@
 import http from '@/utilsjs/http.js';
-export const home = ()=>http("get","/abc/Service/callback.mi/Showtime/LocationMovies.api?locationId=290&t=2019411102990114")
+export const home = ()=>http("get","/api/Service/callback.mi/Showtime/LocationMovies.api?locationId=290&t=2019411102990114")
